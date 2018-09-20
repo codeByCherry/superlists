a0be3ff77a57ca27a67c9e8b4dbcb8bf73cb7ebc
codyByCherry <373466869@qq.com>
2018-09-20 17:41:28 +0800

    增加模型测试：模型的保存和读取

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-from django.urls import resolve
-from .views import home_page
+from .models import Item
 
 
 class SmokeTest(TestCase):
@@ -20,4 +19,27 @@ class SmokeTest(TestCase):
 
         self.assertContains(response, item_text)
         self.assertTemplateUsed(response, 'lists/home_page.html')
+
+
+class ItemModelTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_save_and_retrieving_items(self):
+        item_1 = Item.objects.create(text="#1")
+        item_2 = Item.objects.create(text="#2")
+        item_3 = Item.objects.create(text="#3")
+
+        saved_items = Item.objects.all()
+
+        self.assertIn(item_1, saved_items)
+        self.assertIn(item_2, saved_items)
+        self.assertIn(item_3, saved_items)
+
+        saved_item_1 = Item.objects.get(pk=1)
+        self.assertEqual(item_1.text, saved_item_1.text)
+
 
