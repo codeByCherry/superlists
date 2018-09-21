63c82629b7136661f71b2a8bc627e502e255ca31
codyByCherry <373466869@qq.com>
2018-09-21 10:34:45 +0800

    完成多用户的功能测试   现在的测试点是每个用户都可以保存自己的待办事项，且 没个待办列表的 url 是唯一的

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -67,3 +67,51 @@ class NewVisitorTest(LiveServerTestCase):
         self.input_todo_item(item_2)
         self.wait_for_row_in_list_table(item_1)
         self.wait_for_row_in_list_table(item_2)
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+
+        # 用户1 输入待办事项
+        tony_item1 = "#tony_item1"
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys(tony_item1)
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(tony_item1)
+
+        tony_url = self.browser.current_url
+        self.assertRegex(tony_url, '/lists/.+')
+
+        # 现在另外一个用户继续输入内容
+        # 断言: 之前用户输入的内容不在该页面显示
+        # 断言: 现在的输入显示在该页面中
+        # 断言: 两个用户的 url 不同
+
+        # # 为了防止两个用户之间的 cookie 的影响,
+        # # 所以关闭了浏览器后，重新开启该浏览器
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        andy_item1 = "#andy_item1"
+        andy_item2 = "#andy_item2"
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys(andy_item1)
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(andy_item1)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys(andy_item2)
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(andy_item2)
+
+        # 断言浏览器的 url 符合 REST 风格
+        andy_url = self.browser.current_url
+        self.assertRegex(andy_url, '/lists/.+')
+        # 断言: 之前用户输入的内容不在该页面显示
+        lists_text = self.browser.find_element_by_id('id_list_table').text
+        self.assertNotIn(tony_item1, lists_text)
+        # 断言: 现在的输入显示在该页面中
+        self.wait_for_row_in_list_table(andy_item1)
+        self.wait_for_row_in_list_table(andy_item2)
+        # 断言: 两个用户的 url 不同
+        self.assertNotEqual(tony_url, andy_url)
